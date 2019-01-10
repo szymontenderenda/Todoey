71ab04880aafe39b7cdf1ce11abe34bd5492370d
Szymon Tenderenda <tenderendaszymon@gmail.com>
2019-01-10 14:59:57 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Szymon Tenderenda on 10/01/2019.
+//  Copyright © 2019 Szymon Tenderenda. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
